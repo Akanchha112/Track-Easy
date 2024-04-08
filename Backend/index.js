f6b0594f472350165300408ab009bcbca1920127
Akanchha <sakanchha111@gmail.com>
2024-04-08 16:16:34 +0530

    project initialization

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+
+const app=express();
+
+app.post('/signup',()=>{
+
+})
+app.post('signin',()=>{
+
+})
+app.post('/todo',()=>{
+
+})
+app.get('/todos',()=>{
+
+})
+app.delete('/delete',()=>{
+
+})
+app.listen(3000,()=>{console.log("Server is running")});
---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Track Easy
+A todo application to track down all your todos.
+
+
+
+
+## Features
+
+- Light/dark mode toggle
+- User sign up/in
+- Responsive
+
+
